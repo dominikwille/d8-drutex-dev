095623ca1f89fd683976be09ac2bfc51453ffe83
Dominik Wille <dominik.wille@fu-berlin.de>
2013-10-10 17:26:28 +0200

    init Drutex class

---
+++ drutex.class.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Drupal\drutex;
+
+class Drutex {
+  function __construct() {
+    dpm("new class");
+  }
+}
--- lib/Drupal/drutex/Plugin/Filter/FilterDrutex.php
+++ lib/Drupal/drutex/Plugin/Filter/FilterDrutex.php
@@ -10,14 +10,15 @@ namespace Drupal\drutex\Plugin\Filter;
 use Drupal\filter\Annotation\Filter;
 use Drupal\Core\Annotation\Translation;
 use Drupal\filter\Plugin\FilterBase;
+use Drupal\drutex\Drutex;
 
 /**
  * Provides a filter to display any HTML as plain text.
  *
  * @Filter(
- *   id = "filter_drutex",
+ *   id = "filter_drutex_math",
  *   module = "drutex",
- *   title = @Translation("Drutex"),
+ *   title = @Translation("Drutex math"),
  *   type = FILTER_TYPE_TRANSFORM_IRREVERSIBLE,
  *   weight = -10
  * )
@@ -30,18 +31,11 @@ class FilterDrutex extends FilterBase {
   public function process($text, $langcode, $cache, $cache_id) {
     $text = str_replace('foo', 'bar', $text);
     dpm($text);
+
+    $foo = new Drutex();
     return $text;
   }
 
-  /* TODO */
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getHTMLRestrictions() {
-    // Nothing is allowed.
-    return array('allowed' => array());
-  }
 
   /**
    * {@inheritdoc}
